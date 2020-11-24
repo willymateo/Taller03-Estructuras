745f4706cfcd709e24454177da9130d4ab2492e5
willymateo <wjmateo@espol.edu.ec>
2020-11-24 15:13:14 -0500

    Paquete ubicacion

---
+++ src/ubicacion/Coordenada.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ubicacion;
+
+/**
+ *
+ * @author Willy Mateo
+ */
+public class Coordenada {
+    private double latitud;
+    private double longitud;
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLatitud(double latitud) {
+        this.latitud = latitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+    
+}
---
+++ src/ubicacion/PuntoReferencia.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ubicacion;
+
+/**
+ *
+ * @author Willy Mateo
+ */
+public class PuntoReferencia {
+    private Coordenada coord;
+    private double puntaje;
+
+    public Coordenada getCoord() {
+        return coord;
+    }
+
+    public double getPuntaje() {
+        return puntaje;
+    }
+
+    public void setPuntaje(double puntaje) {
+        this.puntaje = puntaje;
+    }
+    
+}
